90d3fd111d6c52be71ae6887bb67c1b93fab4bb6
mahdib9 <mahdib9@users.noreply.github.com>
2016-02-13 13:04:56 +0000

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -208,6 +208,7 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
+    "id",
     "time",	
     "onservice",
     "welcome",
